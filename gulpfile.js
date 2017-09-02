b667fe84d2802d5478b9cad6e93db6f58493272e
big-boris <b.g.demchenko@yandex.ru>
2017-09-02 17:48:45 +0300

    Подготовка репозитория

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp'),
+    tinypng = require('gulp-tinypng');
+
+var nm = './node_modules/';
+
+gulp.task('tinypng', function() {
+  return gulp.src('dev/img/**/*')
+    .pipe(tinypng('API_KEY')) //key of webdb81
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('dev/img/**/*', ['tinypng']);
+});
+
+gulp.task('default', [
+  'tinypng',
+  'watch'
+]);
